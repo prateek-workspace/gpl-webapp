15ccad5ed393a02a3683c7035be91e040449cef0
Prateek <workspace.infoga@gmail.com>
2023-10-02 09:36:20 +0530

    web

--- home/views.py
+++ home/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def parent(request):
+    return render(request,"parent.html")
+
+def login(request):
+    return render(request,"login.html")
+
 def index(request):
     return render(request,"index.html")
